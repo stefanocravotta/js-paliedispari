02d4e8777f17f103050557aca832159d4ab7e908
stefanocravotta <stefano.cravotta8@gmail.com>
2022-04-20 14:17:04 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+/* 
+1 - Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+Bonus: L’inserimento avviene tramite un campo input 
+
+2 - Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+Bonus: L’inserimento avviene tramite un campo input
+*/
