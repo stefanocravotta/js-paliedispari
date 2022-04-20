95ddf1da1c1999143fa3eb1bdcb97645ebab368e
stefanocravotta <stefano.cravotta8@gmail.com>
2022-04-20 16:31:24 +0200

    2 pari e dispari + bonus

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
 /* 
-1 - Palidroma
+1 - Palindromo
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 Bonus: L’inserimento avviene tramite un campo input 
@@ -15,7 +15,7 @@ Dichiariamo chi ha vinto.
 Bonus: L’inserimento avviene tramite un campo input
 */
 
-
+// 1 Palindromo
 const btnPalindromo = document.getElementById("btn-palindromo");
 let output = document.getElementById("risultato");
 
@@ -37,10 +37,74 @@ btnPalindromo.addEventListener('click', function() {
 })
 
 
+
+// 2 PARI E DISPARI
+
+const btnPariDispari = document.getElementById("btn-pari-dispari");
+let outputPariDispari = document.getElementById("risultato-pari-dispari");
+const numeroMin = 1;
+const numeroMax = 5;
+let verifica = false;
+let scelta = "pari";
+let vittoria = true;
+
+btnPariDispari.addEventListener('click', function(){
+    
+    const numeroUtente = document.getElementById("pari-dispari").value ;
+    const numeroPc = getRandomNumber(numeroMin, numeroMax);
+    const sceltaPariDispari = document.getElementById("scelta").value;
+
+    pariDispari(numeroPc, parseInt(numeroUtente));
+
+
+    if(verifica && sceltaPariDispari === "pari"){
+        outputPariDispari.innerHTML = `La somma del numero scelto da te "${numeroUtente}" <br> e il numero random scelto dal computer "${numeroPc}" <br> è = ${pariDispari(numeroPc, parseInt(numeroUtente))} <br> quindi HAI VINTO!!!!`;
+        console.log(verifica, scelta);
+    }else if(verifica && sceltaPariDispari === "dispari"){
+        outputPariDispari.innerHTML = `La somma del numero scelto da te "${numeroUtente}" <br> e il numero random scelto dal computer "${numeroPc}" <br> è = ${pariDispari(numeroPc, parseInt(numeroUtente))} <br> quindi HAI PERSO!!!!`;
+        console.log(verifica, scelta);
+    }else if(!(verifica) && sceltaPariDispari === "dispari"){
+        outputPariDispari.innerHTML = `La somma del numero scelto da te "${numeroUtente}" <br> e il numero random scelto dal computer "${numeroPc}" <br> è = ${pariDispari(numeroPc, parseInt(numeroUtente))} <br> quindi HAI VINTO!!!!`;
+        console.log(verifica, scelta);
+    }else if(!(verifica) && sceltaPariDispari === "pari"){
+        outputPariDispari.innerHTML = `La somma del numero scelto da te "${numeroUtente}" <br> e il numero random scelto dal computer "${numeroPc}" <br> è = ${pariDispari(numeroPc, parseInt(numeroUtente))} <br> quindi HAI PERSO!!!!`;
+        console.log(verifica, scelta);
+    }
+
+})
+
 /* FUNCTIONS */
+
+// 1 Palindromo
 
 function reverse(word) {
     
     return word.split("").reverse().join("");
 
  }
+
+// 2 PARI E DISPARI
+
+function getRandomNumber(min, max){
+
+    return  Math.floor(Math.random() * (max - min +1) + min );
+  
+  }
+
+function pariDispari(num1,num2) {
+    
+    let sum = num1 + num2;
+
+    console.log(sum);
+
+    if(sum % 2){
+        verifica = false;
+        scelta = "dispari";
+    }else{
+        verifica = true; 
+        scelta = "pari";
+       }
+
+    return sum;
+
+  }
